5fbff25c29ea5055674441e8c8749b1cb17f726c
Ray Cook <rayscook@gmail.com>
2023-05-15 10:19:26 -0400

    increased height of buywidget on mobile

--- src/components/BuyWidget.js
+++ src/components/BuyWidget.js
@@ -3,7 +3,7 @@ import React from 'react';
 const BuyWidget = () => {
     return (
         <iframe
-            className="w-full h-full"
+            className="w-full min-h-[500px]"
             allow="clipboard-read *; clipboard-write *; web-share *; accelerometer *; autoplay *; camera *; gyroscope *; payment *; geolocation *"
             src="https://flooz.xyz/embed/trade?swapDisabled=false&swapToTokenAddress=0x014550A34149231b8432B8dD210fADd81b1d2668&swapLockToToken=false&onRampDisabled=false&onRampAsDefault=false&onRampDefaultAmount=69&onRampTokenAddress=eth&stakeDisabled=true&network=eth&lightMode=false&primaryColor=%23c7df16&backgroundColor=transparent&roundedCorners=32&padding=32&refId=FKCt0Z"
         ></iframe>
